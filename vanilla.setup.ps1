9f05330fbc56f5dc9dd673b7a294fde53666efb8
Matthew Lariviere <malarivi@cisco.com>
2020-12-28 23:55:13 -0500

    dirty upgrades

--- vanilla.setup.ps1
+++ vanilla.setup.ps1
@@ -9,3 +9,6 @@ if (!(Test-Path -Path "$env:ProgramData\Chocolatey")) {
 
 # for each package in the list run install
 Get-Content ".\packages" | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{choco install -y $_}
+
+#This is dirty but it upgrades everything
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
